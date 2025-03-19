e9e5c08ef83b0398593198c863831bac9866cd96
AditInx <adit.s@inhertix.com>
2025-03-19 12:40:36 +0530

    made memory storage with base 64

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,26 +1,35 @@
 import express from 'express';
-const router = express.Router();
 import Student from '../models/student.js';
 import multer from 'multer';
 
-//Set up multer to store files in upload folder
-const storage = multer.diskStorage({
-    destination: (req,file,cb)=>{
-        cb(null, 'uploads/');
-    },
-    filename: (req,file,cb)=>{
-        const suffix = Date.now();
-        cb(null,suffix + '-' + file.originalname);
-    }
-})
+const router = express.Router();
 
-const upload = multer({storage});
+//Set up multer to store files in upload folder
+// const storage = multer.diskStorage({
+//     destination: (req,file,cb)=>{
+//         cb(null, 'uploads/');
+//     },
+//     filename: (req,file,cb)=>{
+//         const suffix = Date.now();
+//         cb(null,suffix + '-' + file.originalname);
+//     }
+// })
+
+// Use memory storage (for small images)
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.post('/create', upload.single('photo'), async (req, res) => {
     try {
         const { name, age, email, phone, address } = req.body;
-        const photopath = req.file ? req.file.path : null; // Get the file path if uploaded
-        const newStudent = new Student({ name, age, email, phone, address,photo: photopath });
+        
+        if (!req.file) {
+            return res.status(400).json({ error: 'Photo upload failed' });
+        }
+
+        const photoBase64 = req.file.buffer.toString('base64');
+
+        const newStudent = new Student({ name, age, email, phone, address, photo: photoBase64 });
 
         await newStudent.save();
 
@@ -28,8 +37,9 @@ router.post('/create', upload.single('photo'), async (req, res) => {
             message: 'Student created successfully', 
             student: newStudent 
         });
+
     } catch (error) {
-        console.log(error);
+        console.error(error);
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 });
